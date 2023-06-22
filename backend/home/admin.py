1c955ce8aa50bb605c51c089f49980654549aa00
crowdbotics-dev <engineering@crowdbotics.com>
2023-06-22 16:02:44 +0000

    Update models

--- backend/home/admin.py
+++ backend/home/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Xnjui
+admin.site.register(Xnjui)
 
 # Register your models here.
--- backend/home/models.py
+++ backend/home/models.py
@@ -1,1 +1,5 @@
+from django.conf import settings
 from django.db import models
+class Xnjui(models.Model):
+    'Generated Model'
+    mjuuy = models.BigIntegerField()
